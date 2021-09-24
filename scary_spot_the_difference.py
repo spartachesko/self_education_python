30193bd3cece3743206ab8d1873e7922f695a977
spartak <fichker@gmail.com>
2021-09-24 13:03:03 +0300

    scary_spot_the_difference.py : 1_commit

---
+++ scary_spot_the_difference.py
@@ -0,0 +1,9 @@
+import pygame
+from time import sleep
+from random import randrange
+pygame.init()
+
+width = pygame.display.Info().current_w
+height = pygame.display.Info().current_h
+screen = pygame.display.set_mode((width,height))
+pygame.quit()
